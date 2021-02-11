b1bf9f54dc3e59d4899ee3d2bb632507db6e31ad
Sxtanna <ranald.taylor@gmail.com>
2021-02-11 15:07:38 -0500

    added replenish enchant section

--- src/main/java/de/seekyml/farming/config/AdvancedFarmingConfiguration.java
+++ src/main/java/de/seekyml/farming/config/AdvancedFarmingConfiguration.java
@@ -6,6 +6,7 @@ import ch.jalu.configme.migration.PlainMigrationService;
 import ch.jalu.configme.properties.Property;
 import ch.jalu.configme.resource.YamlFileResource;
 import de.seekyml.farming.config.sections.PluginSettings;
+import de.seekyml.farming.config.sections.enchants.ReplenishSettings;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
@@ -16,7 +17,7 @@ public final class AdvancedFarmingConfiguration extends SettingsManagerImpl {
     public AdvancedFarmingConfiguration(final Plugin plugin) {
         super(new YamlFileResource(new File(plugin.getDataFolder(), "config.yml").toPath()),
 
-                ConfigurationDataBuilder.createConfiguration(PluginSettings.class),
+                ConfigurationDataBuilder.createConfiguration(PluginSettings.class, ReplenishSettings.class),
 
                 new PlainMigrationService());
     }
